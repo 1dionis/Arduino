e534cac45ee59dab7ef591c01f2d0b6d89f285e9
1dionis <sutkin.dionis@gmail.com>
2026-01-28 22:31:23 +0300

    Update sketch_jan23c.ino

--- CodeArduino/sketch_jan23c/sketch_jan23c.ino
+++ CodeArduino/sketch_jan23c/sketch_jan23c.ino
@@ -1,26 +1,15 @@
-#include <AccelStepper.h>
 #include <Servo.h>
-#include <LedControl.h>
-#include <LiquidCrystal.h>
 
 Servo myServo;
 
-
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const int buttonPin1 = 8;
-const int buttonPin2 = 13;
-const int servoPin = 10;
+const int buttonPin = 2;
+const int servoPin  = 9;
 
 void setup() {
-pinMode(buttonPin1, INPUT_PULLUP);
-myServo.attach(servoPin);
-
-
-
-
+  pinMode(buttonPin, INPUT_PULLUP); // кнопка на GND
+  myServo.attach(servoPin);
 }
 
 void loop() {
-myServo.write(!digitalRead(buttonPin1)*180);
-
+  myServo.write(digitalRead(buttonPin) * 90);
 }
