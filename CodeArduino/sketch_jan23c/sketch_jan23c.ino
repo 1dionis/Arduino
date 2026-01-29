1c4ed8dec8c1d095825f41ba177e40e61e4bf2e5
Lasckuer <113969569+Lasckuer@users.noreply.github.com>
2026-01-29 18:07:27 +0300

    1

--- CodeArduino/sketch_jan23c/sketch_jan23c.ino
+++ CodeArduino/sketch_jan23c/sketch_jan23c.ino
@@ -1,46 +1,15 @@
-#include <SPI.h>
-#include <MFRC522.h>
-
-// Ваши пины согласно предыдущей схеме
-#define RST_PIN         7          
-#define SS_PIN          10         
-
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Создаем объект модуля
+#include <IRremote.h>
 
 void setup() {
   Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
-
-  Serial.println(F("--- Тест связи с RC522 ---"));
-  
-  // Выполняем самодиагностику
-  mfrc522.PCD_DumpVersionToSerial(); 
+  IrReceiver.begin(A0, ENABLE_LED_FEEDBACK); // A0 — ваш пин
+  Serial.println("Ozidanie...");
 }
 
 void loop() {
-  // Проверка: поднесена ли новая карта?
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
+  if (IrReceiver.decode()) {
+    Serial.print("COD knopki: ");
+    Serial.println(IrReceiver.decodedIRData.command, HEX); 
+    IrReceiver.resume(); 
   }
-
-  // Проверка: можно ли считать данные с карты?
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  // Если мы здесь — карта обнаружена!
-  Serial.print(F("Ура! Ключ обнаружен. UID карты:"));
-
-  // Выводим UID в шестнадцатеричном формате
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-  }
-  
-  Serial.println();
-  Serial.println(F("------------------------------------"));
-
-  // Остановка чтения (чтобы не дублировать вывод одной и той же карты)
-  mfrc522.PICC_HaltA();
 }
