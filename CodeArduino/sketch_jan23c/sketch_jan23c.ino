8155d6f85bffe13ce91caf143d519fda885b7c35
1dionis <sutkin.dionis@gmail.com>
2026-01-29 18:04:03 +0300

    Update sketch_jan23c.ino

--- CodeArduino/sketch_jan23c/sketch_jan23c.ino
+++ CodeArduino/sketch_jan23c/sketch_jan23c.ino
@@ -2,30 +2,24 @@
 #include <Servo.h>
 #include <LedControl.h>
 #include <LiquidCrystal.h>
-#include <SPI.h>
-#include <MFRC522.h>
-
-#define SS_PIN 9
-#define RST_PIN 10
 
 Servo myServo;
-MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-
-const int buttonPin2 = A0;
+LiquidCrystal lcd(7, 6 , 5, 4, 3, 2);
+const int irPin = A0;
+const int buttonPin2 = 9;
 const int servoPin = 8;
 
+
 bool servoState = 0;
-bool lastCardState = 0;
+bool lastLight = 0;
 bool lastButton2 = HIGH;
 
-// UID карты (замени на свой)
-byte allowedUID[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+IRrecv irrecv(irPin);
+decode_results results;
 
 void setup() {
-  pinMode(buttonPin2, INPUT_PULLUP);
-
+ pinMode(buttonPin2, INPUT_PULLUP);
   myServo.attach(servoPin);
   myServo.write(0);
 
@@ -34,47 +28,25 @@ void setup() {
   lcd.setCursor(0, 0);
 
   Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
+
+irrecv.enableIRIn();
 }
 
 void loop() {
+  bool irState = false;
 
-  // ===== RFID вместо фоторезистора =====
-  bool cardPresent = false;
-
-  if (mfrc522.PICC_IsNewCardPresent() &&
-      mfrc522.PICC_ReadCardSerial()) {
-
-    cardPresent = true;
-
-    bool uidMatch = true;
-    for (byte i = 0; i < 4; i++) {
-      if (mfrc522.uid.uidByte[i] != allowedUID[i]) {
-        uidMatch = false;
-        break;
-      }
-    }
-
-    if (uidMatch) {
-      servoState ^= (cardPresent & !lastCardState);
-      myServo.write(servoState * 180);
-      delay(700);
-      myServo.write(0);
-    }
-
-    mfrc522.PICC_HaltA();
+  if(irrecv.decode(&results)){
+irState = true;
+irrecv.recume();
   }
 
-  lastCardState = cardPresent;
+servoState ^= (irState & !lastIR);
+lastrIR = irState;
+myServo.write(servoState * 180);
 
-  // ===== КНОПКА =====
-  if (digitalRead(buttonPin2) == LOW && lastButton2 == HIGH) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Button pressed");
-    delay(300);
+if (digitalRead(buttonPin2) == HIGH) {
+    lcd.println("hdsvbdu");
+    delay(300); // чтобы не спамило
   }
 
-  lastButton2 = digitalRead(buttonPin2);
 }
