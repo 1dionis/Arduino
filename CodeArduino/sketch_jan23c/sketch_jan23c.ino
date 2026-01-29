ab9ead7447950dbf165ee8efc1209b305a48ba76
Lasckuer <113969569+Lasckuer@users.noreply.github.com>
2026-01-29 22:05:54 +0300

    Update sketch_jan23c.ino

--- CodeArduino/sketch_jan23c/sketch_jan23c.ino
+++ CodeArduino/sketch_jan23c/sketch_jan23c.ino
@@ -1,16 +1,25 @@
 #include <IRremote.h>
 
 void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW); // Принудительно гасим L
+  
   Serial.begin(9600);
-  // Пробуем инициализацию на 11 пине
-  IrReceiver.begin(11, ENABLE_LED_FEEDBACK); 
-  Serial.println("Система запущена. Жду сигнал...");
+  // Инициализируем приемник на пине A0 БЕЗ светодиодной индикации
+  IrReceiver.begin(A0, DISABLE_LED_FEEDBACK); 
+  Serial.println("Система очищена. Жду пульт на A0...");
 }
 
 void loop() {
   if (IrReceiver.decode()) {
-    Serial.print("УРА! Поймал код: ");
+    Serial.print("УРА! Код получен: ");
     Serial.println(IrReceiver.decodedIRData.command, HEX);
-    IrReceiver.resume();
+    
+    // Мгновенно мигнем светодиодом L только при получении сигнала
+    digitalWrite(13, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    `
+    IrReceiver.resume(); 
   }
 }
